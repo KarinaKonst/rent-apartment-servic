730327cb5a4ae36ec5bdc347fadf6b2c8b63c22d
KarinaKonst <131575970+KarinaKonst@users.noreply.github.com>
2023-08-24 15:10:55 +0300

    create script for revert

---
+++ src/test/java/com/example/fasadapi/FasadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fasadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasadApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
